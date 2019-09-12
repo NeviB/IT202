1ed94e94251678659f8c4a76abbd8ea148093fad
your git username <your email>
2019-09-12 11:37:02 -0400

    test

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?
-$database="";
-$username="";
-$password="";
-$host="";
+$host="sql1.njit.edu";//your specific server
+$database="nab54";
+$username="nab54";
+$password="affirm86";
 ?>
---
+++ initDb.php
@@ -0,0 +1,6 @@
+<?php
+require('config.php');
+
+echo $host;
+?>
+
