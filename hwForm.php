895550dba7fe143351ee7e0691b3440501b714bd
NeviB <bunag.nevin@gmail.com>
2019-10-04 13:23:03 -0400

    new file

---
+++ hwForm.php
@@ -0,0 +1,35 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors',1);
+error_reporting(E_ALL);
+function getName(){
+	if(isset($_GET['name'])){
+		echo "<p>Hello, " . $_GET['name'] . "</p>";
+	}
+}
+?>
+
+<html>
+<head></head>
+
+<body><?php getName();?>
+<form mode="GET" action="#">
+<input name = "Username" type = "text" placeholder="Enter Username"/>
+<!--new content-->>
+<input name="password" type= "password" placeholder="Enter password"/>
+<input name="password2" type= "password" placeholder="Confirm password"/>
+<input type="submit" value="Try it"/>
+<input type="reset" value="Clear Form"/>
+</form>
+</body>
+</html>
+
+<?php
+if(isset($_GET)){
+	echo "<br><pre>" . var_export($_GET, true) . "</pre><br>";
+
+if(isset($_GET['password']))!==(isset()$_GET['password2']){
+echo "<p>"your passwords are not the same " "</p>";
+
+}
+}
