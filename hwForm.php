2847c7cd373fd44bb920fba90d5b99d45147a5e2
NeviB <bunag.nevin@gmail.com>
2019-10-08 00:04:09 -0400

    This is my homework

--- hwForm.php
+++ hwForm.php
@@ -1,33 +1,30 @@
-<?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors',1);
-error_reporting(E_ALL);
-function getName(){
-	if(isset($_GET['name'])){
-		echo "<p>Hello, " . $_GET['name'] . "</p>";
-	}
-}
-?>
+
 
 <html>
-<head></head>
-
-<body><?php getName();?>
-<form mode="GET" action="#">
-<input name = "Username" type = "text" placeholder="Enter Username"/>
-<!--new content-->>
-<input name="password" type= "password" placeholder="Enter password"/>
-<input name="password2" type= "password" placeholder="Confirm password"/>
-<input type="submit" value="Try it"/>
-<input type="reset" value="Clear Form"/>
-</form>
-</body>
+  <body>
+  <form method="post">
+      Username: <br>
+      <input type="text" name="username"> <br>
+      Password: <br>
+      <input type="password" name="password"> <br>
+      Confirm Password: <br>
+      <input type="password" name="confirmPassword"> <br>
+      
+     
+      
+      <input type="submit">
+    </form>
+  </body>
 </html>
-
 <?php
-if(isset($_GET)){
-	echo "<br><pre>" . var_export($_GET, true) . "</pre><br>";
-
-
-}
-}
+  $username =  $_POST["username"];
+  $password =  $_POST["password"];
+  $confirmPassword =  $_POST["confirmPassword"];
+  
+  if($password == $confirmPassword){
+    echo "YOUR PASSWORDS ARE THE SAME";
+  }
+  else{
+    echo "YOUR PASSWORDS DO NOT MATCH!!!";
+  }
+?>
