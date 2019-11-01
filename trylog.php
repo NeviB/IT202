8a91e10188fcfb742406a66c150055071ac42c83
NeviB <bunag.nevin@gmail.com>
2019-10-31 22:12:05 -0400

    This is the Login Page

---
+++ trylog.php
@@ -0,0 +1,25 @@
+<!DOCTYPE>
+
+
+
+<h1> Login </h1>
+<body>
+    <div id="hiii">
+        <form action="loginME.php" method="POST">
+          <p>
+            <label>Username:</label>
+            <input type="text" id="username" name="username" />
+          </p>
+          <p>
+            <label>Password:</label>
+            <input type="password" id="password" name="password" />
+          </p>
+          <p>
+              <input type="submit" id="btn" value="Login">
+            </p>
+
+          </form>>
+        </div>>
+
+      </body>
+      </html>
